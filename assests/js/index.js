083b776aaf87eaf2fead3cb817ef9cbd1115dd03
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-07-12 10:05:49 -0700

    fix linters

--- assests/js/index.js
+++ assests/js/index.js
@@ -47,7 +47,7 @@ if (
   Book.setUI(bookList, itemList());
 }
 
-// Website Navigation
+// ##################### Website Navigation
 
 const navList = document.querySelector('#nav-list');
 const navAddNew = document.querySelector('#nav-add-new');
@@ -85,7 +85,7 @@ navContact.addEventListener('click', () => {
   contactSection.classList.remove('d-none');
 });
 
-//Mobile navbar
+// ################ Mobile navbar ############
 
 const hamburgerMenu = document.querySelector('.hamburger');
 const navBar = document.querySelector('nav');
